cbfa08692354241000c360ca6a3f248fb0aeb5ff
bradley vangelder <r0784535@student.thomasmore.be>
2021-12-10 10:27:12 +0100

    adding routes to edge

--- src/main/java/fact/it/edgeservice/controller/BookQuoteController.java
+++ src/main/java/fact/it/edgeservice/controller/BookQuoteController.java
@@ -6,14 +6,12 @@ import fact.it.edgeservice.model.Quote;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.lang.reflect.Array;
 import java.util.List;
 
 @RestController
@@ -28,8 +26,7 @@ public class BookQuoteController {
     private RestTemplate restTemplate;
 
     @GetMapping("/bookquotes/{ISBN}")
-    public BookQuotes getRankingsByUserId(@PathVariable String ISBN){
-
+    public BookQuotes getBookQuotesByISBN(@PathVariable String ISBN){
         Book book =
                 restTemplate.getForObject("http://" + bookServiceBaseUrl + "/book/{ISBN}",
                         Book.class, ISBN);
@@ -42,5 +39,28 @@ public class BookQuoteController {
         List<Quote> quotes = responseEntityReviews.getBody();
 
         return new BookQuotes(book.getTitle(),book.getISBN(), quotes);
+    }
+
+    @PutMapping("/bookquotes/quote")
+    public Quote edit(@RequestParam String quoteId, @RequestParam String newQuote){
+        Quote quote =
+                restTemplate.getForObject("http://" + quoteServiceBaseUrl + "/quote/" + quoteId,
+                        Quote.class);
+        quote.setQuote(newQuote);
+
+        ResponseEntity<Quote> responseEntityReview =
+                restTemplate.exchange("http://" + quoteServiceBaseUrl + "/quote/" + quoteId,
+                        HttpMethod.PUT, new HttpEntity<>(quote), Quote.class);
+
+        Quote retrievedReview = responseEntityReview.getBody();
+
+        return retrievedReview;
+    }
+
+    @DeleteMapping("/bookquotes/quote")
+    public ResponseEntity delete(@RequestParam String quoteId){
+        restTemplate.delete("http://" + quoteServiceBaseUrl + "/quote/" + quoteId);
+
+        return ResponseEntity.ok().build();
     }
 }
