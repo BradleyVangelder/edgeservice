b6f08be9c1e220097b5caabff8de6e990950357e
bradley vangelder <r0784535@student.thomasmore.be>
2022-01-02 22:25:12 +0100

    adding cors

--- src/main/java/fact/it/edgeservice/controller/BookQuoteController.java
+++ src/main/java/fact/it/edgeservice/controller/BookQuoteController.java
@@ -16,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.HashMap;
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class BookQuoteController {
     @Value("${bookservice.baseurl}")
