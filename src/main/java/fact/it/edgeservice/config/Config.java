82ea08f23bcdef8ecef5484496d7b73044519aef
bradley vangelder <r0784535@student.thomasmore.be>
2022-01-06 16:00:30 +0100

    deleting connection with jms

--- src/main/java/fact/it/edgeservice/config/Config.java
+++ src/main/java/fact/it/edgeservice/config/Config.java
@@ -12,37 +12,37 @@ import javax.jms.Queue;
 @Configuration
 public class Config {
 
-    @Value("${activemq.broker-url}")
-    private String brokerUrl;
-
-    @Bean
-    public Queue queue() {
-        return new ActiveMQQueue("test.queue");
-    }
-    @Bean
-    public Queue editQuote() {
-        return new ActiveMQQueue("editQuote.queue");
-    }
-    @Bean
-    public Queue createQuote() {
-        return new ActiveMQQueue("createQuote.queue");
-    }
-    @Bean
-    public Queue deleteQuote() {
-        return new ActiveMQQueue("deleteQuote.queue");
-    }
-
-    @Bean
-    public ActiveMQConnectionFactory activeMQConnectionFactory() {
-        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory();
-        factory.setBrokerURL(brokerUrl);
-        factory.setUserName("admin");
-        factory.setPassword("admin");
-        return factory;
-    }
-
-    @Bean
-    public JmsTemplate jmsTemplate() {
-        return new JmsTemplate(activeMQConnectionFactory());
-    }
+//    @Value("${activemq.broker-url}")
+//    private String brokerUrl;
+//
+//    @Bean
+//    public Queue queue() {
+//        return new ActiveMQQueue("test.queue");
+//    }
+//    @Bean
+//    public Queue editQuote() {
+//        return new ActiveMQQueue("editQuote.queue");
+//    }
+//    @Bean
+//    public Queue createQuote() {
+//        return new ActiveMQQueue("createQuote.queue");
+//    }
+//    @Bean
+//    public Queue deleteQuote() {
+//        return new ActiveMQQueue("deleteQuote.queue");
+//    }
+//
+//    @Bean
+//    public ActiveMQConnectionFactory activeMQConnectionFactory() {
+//        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory();
+//        factory.setBrokerURL(brokerUrl);
+//        factory.setUserName("admin");
+//        factory.setPassword("admin");
+//        return factory;
+//    }
+//
+//    @Bean
+//    public JmsTemplate jmsTemplate() {
+//        return new JmsTemplate(activeMQConnectionFactory());
+//    }
 }
